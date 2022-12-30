33d6332e5e2c228199278d307136168085399f1a
Андрей Гальчевский <super.rip6000@yandex.ru>
2022-12-30 18:43:01 +0700

    Second changes

--- homework.py
+++ homework.py
@@ -49,6 +49,12 @@ class Training:
 
 class Running(Training):
     """Тренировка: бег."""
+    CALORIES_MEAN_SPEED_MULTIPLIER = 18
+    CALORIES_MEAN_SPEED_SHIFT = 1.79
+    def get_spent_calories(self) -> float:
+        return ((self.CALORIES_MEAN_SPEED_MULTIPLIER * self.get_mean_speed() 
+                + self.CALORIES_MEAN_SPEED_SHIFT)
+                * self.weight / self.M_IN_KM * self.duration)
     pass
 
 
