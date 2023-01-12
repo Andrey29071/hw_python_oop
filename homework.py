d2983ec08154ca487e9227cbf41d4bd9c87bfb8d
Андрей Гальчевский <super.rip6000@yandex.ru>
2023-01-12 20:55:54 +0700

    Обработка кода после ревью

--- homework.py
+++ homework.py
@@ -1,8 +1,8 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from typing import Dict, List, ClassVar
 
 
-@dataclass
+@dataclass (init=True, repr=False, eq=False, order=False, unsafe_hash=False, frozen=False)
 class InfoMessage:
     """Информационное сообщение о тренировке."""
 
@@ -11,18 +11,19 @@ class InfoMessage:
     distance: float
     speed: float
     calories: float
+    
+    message: str = ('Тип тренировки: {training_type}; '
+                    'Длительность: {duration:.3f} ч.; '
+                    'Дистанция: {distance:.3f} км; '
+                    'Ср. скорость: {speed:.3f} км/ч; '
+                    'Потрачено ккал: {calories:.3f}.')
 
     def get_message(self) -> str:
         """Метод возвращает строку сообщения."""
-        message: str = (f'Тип тренировки: {self.training_type}; '
-                        f'Длительность: {self.duration:.3f} ч.; '
-                        f'Дистанция: {self.distance:.3f} км; '
-                        f'Ср. скорость: {self.speed:.3f} км/ч; '
-                        f'Потрачено ккал: {self.calories:.3f}.')
-        return (message)
+        return self.message.format(**asdict(self))
 
 
-@dataclass
+@dataclass (init=True, repr=False, eq=False, order=False, unsafe_hash=False, frozen=False)
 class Training:
     """Базовый класс тренировки."""
 
@@ -44,7 +45,8 @@ class Training:
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        raise NotImplementedError("Требуется определить get_spent_calories()")
+        raise NotImplementedError(f'Требуется определить get_spent_calories() '
+                                  f'в классе {__class__.__name__}')
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
@@ -56,7 +58,7 @@ class Training:
                            )
 
 
-@dataclass
+@dataclass (init=True, repr=False, eq=False, order=False, unsafe_hash=False, frozen=False)
 class Running(Training):
     """Тренировка: бег."""
 
@@ -70,7 +72,7 @@ class Running(Training):
                 / self.M_IN_KM * (self.duration * self.MIN_IN_H))
 
 
-@dataclass
+@dataclass (init=True, repr=False, eq=False, order=False, unsafe_hash=False, frozen=False)
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
 
@@ -92,7 +94,7 @@ class SportsWalking(Training):
                 * self.duration * self.MIN_IN_H)
 
 
-@dataclass
+@dataclass (init=True, repr=False, eq=False, order=False, unsafe_hash=False, frozen=False)
 class Swimming(Training):
     """Тренировка: плавание."""
 
